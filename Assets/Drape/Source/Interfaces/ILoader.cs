92c585921361e9738540068db06099adb6694a22
FullStackForger <fullstackforger@gmail.com>
2017-05-18 15:45:38 +0100

    JSON Loader added

--- Assets/Drape/Source/Interfaces/ILoader.cs
+++ Assets/Drape/Source/Interfaces/ILoader.cs
@@ -1,9 +1,19 @@
-﻿using System.Collections.Generic;
+﻿using Drape.Delegates;
+using Drape.Interfaces;
 
 namespace Drape
 {
     public interface ILoader
     {
-        Dictionary<string, Stat> Load();
+        event OnLoadCompleteHandler OnLoadComplete;
+        event OnLoadErrorHandler OnLoadError;
+
+        void Load<T>(string path) where T : IStat;
     }
+}
+
+namespace Drape.Delegates
+{
+    public delegate void OnLoadCompleteHandler();
+    public delegate void OnLoadErrorHandler(System.Exception e);
 }
---
+++ Assets/Drape/Source/Loaders/JSONLoader.cs
@@ -0,0 +1,30 @@
+﻿using Drape.TinyJson;
+using Drape.Interfaces;
+
+namespace Drape.Loaders
+{
+    public class JSONLoader: LoaderBase, ILoader
+    {
+        public JSONLoader(Registry registry) : base(registry)
+        {
+        }
+
+        public override void Load<T>(string path)
+        {
+            try {
+                string json;
+                json = System.IO.File.ReadAllText(path);
+
+                T[] stats = json.FromJson<T[]>();
+                foreach (T stat in stats) {
+                    Registry.Add<T>(stat);
+                }
+
+                TriggerLoadingComplete();
+            } catch (System.IO.FileNotFoundException  e) {
+                TriggerLoadingError(e);
+            }
+        }
+    }
+
+}

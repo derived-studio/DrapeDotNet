020f9575f79d30c25bbfa94cb5a9da8b490994db
FullStackForger <fullstackforger@gmail.com>
2017-05-18 13:00:45 +0100

    Tidying up

--- Assets/Drape/Source/Slug/SlugParser.cs
+++ Assets/Drape/Source/Slug/SlugParser.cs
@@ -2,18 +2,21 @@
 
 namespace Drape.Slug
 {
+    /// source: http://stackoverflow.com/a/2921135/6096446
+    /// Extension converting string to slug
     public static class SlugParser
     {
         public static string GenerateSlug(this string phrase)
         {
             string str = phrase.RemoveAccent().ToLower();
-            // invalid chars           
+            // remove invalid chars
             str = Regex.Replace(str, @"[^a-z0-9\s-]", "");
             // convert multiple spaces into one space   
             str = Regex.Replace(str, @"\s+", " ").Trim();
             // cut and trim 
             str = str.Substring(0, str.Length <= 45 ? str.Length : 45).Trim();
-            str = Regex.Replace(str, @"\s", "-"); // hyphens   
+            // add hyphens
+            str = Regex.Replace(str, @"\s", "-");
             return str;
         }
 

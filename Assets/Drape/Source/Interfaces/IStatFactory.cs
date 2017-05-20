56d827cab6a8098350fd23874061313b9f2be91d
FullStackForger <fullstackforger@gmail.com>
2017-05-20 01:51:26 +0100

    Stats moved to ./stats

---
+++ Assets/Drape/Source/Interfaces/IStatFactory.cs
@@ -0,0 +1,9 @@
+﻿namespace Drape.Interfaces
+{
+    public interface IStatFactory<TStat, TData>
+        where TStat : IStat
+        where TData : BaseStatData
+    {
+        TStat Create(TData data);
+    }
+}
---
+++ Assets/Drape/Source/Stats/ResourceData.cs
@@ -0,0 +1,16 @@
+﻿namespace Drape
+{
+    [System.Serializable]
+    public class ResourceData : MultistatData
+    {
+        public string capacity;
+        public string output;
+
+        public ResourceData(string code, string name, int value, string capacity, string output)
+            : base(code, name, value, new string[] { capacity, output })
+        {
+            this.capacity = capacity;
+            this.output = output;
+        }
+    }
+}

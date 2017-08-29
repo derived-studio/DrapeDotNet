817cea5e91f59c27c245ccd983730568761b5223
FullStackForger <fullstackforger@gmail.com>
2017-08-29 09:45:34 +0100

    Formatting

--- Assets/Plugins/Drape/Source/Stats/ModifierData.cs
+++ Assets/Plugins/Drape/Source/Stats/ModifierData.cs
@@ -1,41 +1,44 @@
 ﻿using Drape.Slug;
 namespace Drape
 {
-    [System.Serializable]
-    public class ModifierData : BaseStatData
-    {
-        public string stat;
-        public int rawFlat;
-        public float rawFactor;
-        public int finalFlat;
-        public float finalFactor;
+	[System.Serializable]
+	public class ModifierData : BaseStatData
+	{
+		public string stat;
+		public int rawFlat;
+		public float rawFactor;
+		public int finalFlat;
+		public float finalFactor;
 
-        public ModifierData(string name, string stat) : this(name.ToSlug(), name, stat, 0, 0, 0, 0) { }
+		public ModifierData(
+			string name,
+			string stat
+		) : this(name.ToSlug(), name, stat, 0, 0, 0, 0) { }
 
-        public ModifierData(
-            string name,
-            string stat,
-            int rawFlat,
-            float rawFactor,
-            int finalFlat,
-            float finalFactor
-        ) : this(name.ToSlug(), name, stat, rawFlat, rawFactor, finalFlat, finalFactor) { }
+		public ModifierData(
+			string name,
+			string stat,
+			int rawFlat,
+			float rawFactor,
+			int finalFlat,
+			float finalFactor
+		) : this(name.ToSlug(), name, stat, rawFlat, rawFactor, finalFlat, finalFactor) { }
 
-        public ModifierData(
-            string code,
-            string name, 
-            string stat,
-            int rawFlat,
-            float rawFactor,
-            int finalFlat,
-            float finalFactor
-        ) : base(code, name, 0)
-        {
-            this.stat = stat;
-            this.rawFlat = rawFlat;
-            this.rawFactor = rawFactor;
-            this.finalFlat = finalFlat;
-            this.finalFactor = finalFactor;
-        }
-    }
+		public ModifierData(
+			string code,
+			string name,
+			string stat,
+			int rawFlat,
+			float rawFactor,
+			int finalFlat,
+			float finalFactor
+		) : base(code, name, 0)
+		{
+			this.stat = stat;
+			this.rawFlat = rawFlat;
+			this.rawFactor = rawFactor;
+			this.finalFlat = finalFlat;
+			this.finalFactor = finalFactor;
+		}
+	}
 }

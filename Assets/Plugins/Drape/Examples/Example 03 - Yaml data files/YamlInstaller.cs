ab0050b91ffc412746279a504a87aaa1f2fefdab
FullStackForger <fullstackforger@gmail.com>
2017-09-14 17:57:43 +0100

    YamlDotNet lib support with usage example

---
+++ Assets/Plugins/Drape/Examples/Example 03 - Yaml data files/YamlInstaller.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using Drape.Interfaces;
+
+namespace Drape.Eamples.Example3
+{
+	class YamlInstaller<TStat, TStatData> : IInstaller
+		where TStat : IStat
+		where TStatData : BaseStatData
+	{
+		private TStatData[] statDataArr;
+		private YamlLoader yamlLoader = new YamlLoader();
+
+		public YamlInstaller(string data)
+		{
+			UnityEngine.Debug.Log(data);
+			List<TStatData> statData = new List<TStatData>();
+			statData = yamlLoader.FromString<TStatData>(data);
+			statDataArr = statData.ToArray();
+		}
+
+		public void Install(Registry registry)
+		{
+			foreach (TStatData statData in statDataArr) {
+				TStat stat = (TStat)System.Activator.CreateInstance(typeof(TStat), new object[] { (TStatData)statData, registry });
+				registry.Add<TStat>(stat);
+			}
+		}
+	}
+}
